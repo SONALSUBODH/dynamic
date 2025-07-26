c582bf3aacb8fdb8099cb24cc9278ec867b83ae6
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-26 22:25:48 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,3 +14,38 @@ variable "instance_tags" {
     Team  = "DevOps"
   }
 }
+variable "ami_id" {
+  type = string
+
+}
+
+variable "instance_type" {
+  type = string
+
+}
+
+variable "instance_tags" {
+  type = map
+
+}
+
+variable "sg_name" {
+  type = string
+
+ }
+
+variable "sg_description" {
+  type = string
+
+}
+
+variable "vpc_id" {
+  type = string
+
+}
+
+variable "sg_tags" {
+  type = map
+
+}
+  
