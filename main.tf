571cf758fd82147d679233a31cd11317b7bee61f
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-26 23:03:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,27 +10,31 @@ resource "aws_security_group" "tf_sg" {
   description = "var.sg_description"
   vpc_id      = "var.vpc_id"
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
 
+dynamic "ingress" {
+    for_each = each.value.ingress
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
 
+  dynamic "egress" {
+    for_each = each.value.egress
+    content {
+      description = egress.value.description
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = var.sg_tags
+
+
+ tags = var.sg_tags
 
 }
 
