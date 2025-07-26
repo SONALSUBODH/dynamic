8fd22ccd823187f03e1c5429c94812a42e7ff70c
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-26 22:10:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,52 @@
+resource "aws_instance" "demo" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.tf_sg.name]
+  tags            = var.instance_tags
+
+}
+resource "aws_security_group" "tf_sg" {
+  name        = "var.sg_name"
+  description = "var.sg_description"
+  vpc_id      = "var.vpc_id"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = var.sg_tags
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 resource "aws_security_group" "tf_sg" {
   name        = "tf_sg"
   description = "Allow TLS inbound traffic"
